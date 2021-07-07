72958fd91d3da0eb95034ad8a2c4c84ab3eee798
deepakspk <deepaksapkota1991@gmail.com>
2021-07-07 18:57:05 +0545

    database changed

--- bp_ecm/settings.py
+++ bp_ecm/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'bp_ecm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
     }
 }
 
@@ -147,6 +151,6 @@ EMAIL_HOST = 'mail.nepalcanmove.com'
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 EMAIL_PORT = 465
-EMAIL_HOST_USER='hris@nepalcanmove.com'
-EMAIL_HOST_PASSWORD='NepalCan12$'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'support@nepalcanmove.com'
